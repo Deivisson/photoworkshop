1dd3238338d0dbf3f3edf211fc839b2079b08255
Deivisson <deivisson.bruno@gmail.com>
2014-08-30 01:05:07 -0300

    Photos

--- app/controllers/user/photo_likes_controller.rb
+++ app/controllers/user/photo_likes_controller.rb
@@ -2,11 +2,17 @@ class User::PhotoLikesController < User::BaseController
 	before_action :set_photo 
 
 	def like
-		current_user.liked_photos << @photo	unless @photo.nil?
+		if !@photo.nil? && !@photo.admirer_ids.include?(current_user.id)
+			current_user.liked_photos << @photo	
+			@likes_count = @photo.likes.count
+		end
 	end
 
 	def unlike
-		current_user.photo_likes.where(photo_id:@photo.id).first.destroy	unless @photo.nil?
+		if !@photo.nil? && @photo.admirer_ids.include?(current_user.id)
+			current_user.photo_likes.where(photo_id:@photo.id).first.destroy	
+			@likes_count = @photo.likes.count
+		end
 	end
 
 private
