045dfa71b0c429cf8cdeb6dd6b03a668ded8e3f1
deivisson <deivisson.bruno@gmail.com>
2014-11-21 00:51:30 -0200

    Ajuste comentários

--- app/controllers/user/photo_explore_controller.rb
+++ app/controllers/user/photo_explore_controller.rb
@@ -6,6 +6,7 @@ class User::PhotoExploreController < User::BaseController
 
 	def show
 		@photo = Photo.find(params[:photo_id])
+		@photo.update_views! if @photo.user_id != current_user.id
 	end
 
 end
