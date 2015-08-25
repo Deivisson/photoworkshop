cc8b64bdd871e3aebdbdfc46e24cdd7a73c523d3
deivisson <deivisson.bruno@gmail.com>
2015-08-24 23:53:58 -0300

    Menu Profile

--- app/assets/javascripts/common/base.js
+++ app/assets/javascripts/common/base.js
@@ -1,4 +1,12 @@
 $(document).on('ready page:load', function (){
+
+	$("#menu-trigger").click(function(){
+		$('#menu-dropdown').toggleClass('open-menu');
+		// $(document).on('click',function() {
+		// 	$('#menu-dropdown').toggle();
+		// });
+	});
+
 	$(function(){
     if ($("#flash_notice").length > 0 || $("#flash_warning").length > 0 || $("#flash_alert").length > 0)  {
       setTimeout(removeFlashMenssage,7000);
