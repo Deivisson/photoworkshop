88624337841eba1e317824eef92479b0561c66c1
deivisson <deivisson.bruno@gmail.com>
2015-11-10 22:56:59 -0200

    Home

--- app/assets/javascripts/home/welcome.js
+++ app/assets/javascripts/home/welcome.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	$('#welcome-content').css("margin-top",value - $('#welcome-content').height() + 50);
 
     // Default move to top
-    $('html,body').animate({scrollTop: top},250);
+    $('html,body').animate({scrollTop: 0},250);
 
 	$('#how-work-link, #made-for-who-link, #top-link, #professionals-link, #sign-up-link').click(function(event) {
 		var link = $(this);
@@ -19,29 +19,26 @@ $(document).ready(function(){
 
 		$("#menu-home").find("li > a").removeClass("selected");
 		link.addClass("selected");
-        $('html,body').animate({scrollTop: top},250, function(){
-        	
-        	//Efect on click How Work link
-        	if (linkId == "how-work-link")	{
-        		$("#step1").addClass("hover");
-        	} else if(linkId == "professionals-link") {
-        		$("#welcome-header").addClass("dark");
-        		$("#pic-professionals").addClass("showed");
-        		$("#professionals-text-box").addClass("showed-text-box");
-        	}  else if(linkId == "sign-up-link") {
-                $("#sign-up-box").addClass("showed");
-        		$("#welcome-header").addClass("dark");
-                $("#user-full-name").focus();
-        	}
-        		
-        });
-        event.preventDefault();
+    $('html,body').animate({scrollTop: top},250, function(){
+    	
+    	//Efect on click How Work link
+    	if (linkId == "how-work-link")	{
+    		$("#step1").addClass("hover");
+    	} else if(linkId == "professionals-link") {
+    		$("#welcome-header").addClass("dark");
+    		$("#pic-professionals").addClass("showed");
+    		$("#professionals-text-box").addClass("showed-text-box");
+    	}  else if(linkId == "sign-up-link") {
+            $("#sign-up-box").addClass("showed");
+    		$("#welcome-header").addClass("dark");
+            $("#user-full-name").focus();
+    	}
+    });
+    event.preventDefault();
 	});
 
 	//Remover hover class from all .step element
 	$(".step ").mouseenter(function(){
     $(".step").removeClass("hover");
-
-    
   });
 });
