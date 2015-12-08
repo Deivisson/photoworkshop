fa27fa1eae2aa37ab5dabb27487625d9d0381af1
deivisson <deivisson.bruno@gmail.com>
2015-12-08 13:56:20 -0200

    Workshop

--- app/models/workshop_material.rb
+++ app/models/workshop_material.rb
@@ -10,7 +10,9 @@ class WorkshopMaterial < ActiveRecord::Base
                    :path => ":rails_root/public/workshop_material/:class/:attachment/:id/:style/:basename.:extension",
                    :url => "/workshop_material/:class/:attachment/:id/:style/:basename.:extension"
 
+  validates_attachment_size :attachment, less_than: 10.1.megabytes, message: I18n.t("activerecord.errors.messages.file_size",size:10)
   validates_attachment_content_type :attachment, 
-                              :content_type =>['application/pdf', 'application/msword', 'text/plain', /\Aimage\/.*\Z/]
-  
+                              :content_type =>['application/zip','application/pdf', 'application/msword', 
+                                'application/vnd.ms-office', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                'text/plain', /\Aimage\/.*\Z/]
 end
