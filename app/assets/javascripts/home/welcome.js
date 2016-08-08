a7728b60fbd9183dccde8ec9ffcf18557bb8e190
Railander Silva <railanderguilherme@gmail.com>
2016-08-08 20:33:58 -0300

    criação da pagina publica workshop, adicionado internacionalização

--- app/assets/javascripts/home/welcome.js
+++ app/assets/javascripts/home/welcome.js
@@ -5,9 +5,9 @@ $(document).on('ready page:load', function (){
 
   // Default move to top
   $('html,body').animate({scrollTop: 0},250);
-	$('#how-work-link, #made-for-who-link, #top-link, #professionals-link,#sign-up-link, #welcome-contact-us-link, #welcome-contact-us-link2').click(function(event) {
+	$('#how-work-link, #made-for-who-link, #top-link, #professionals-link,#sign-up-link, #welcome-contact-us-link, #welcome-contact-us-link2, #how-workshop-link').click(function(event) {
    
-    $(".how_work,.made-for-who,.professionals,.sign-up-container,.contact-us").css("visibility","visible");
+    $(".how_work,.workshops,.made-for-who,.professionals,.sign-up-container,.contact-us").css("visibility","visible");
 
     link = $(this);
 		var linkId = link.attr("id");
@@ -26,7 +26,11 @@ $(document).on('ready page:load', function (){
     		$("#welcome-header").addClass("dark");
     		$("#pic-professionals").addClass("showed");
     		$("#professionals-text-box").addClass("showed-text-box");
-    	}  else if(linkId == "sign-up-link") {
+    	} else if(linkId == "how-workshop-link") {
+        $("#step1").addClass("hover");
+        $("#welcome-header").addClass("dark");
+      }
+        else if(linkId == "sign-up-link") {
           $("#sign-up-box").addClass("showed");
     		  $("#welcome-header").addClass("dark");
           $("#user-full-name").focus();
@@ -53,7 +57,7 @@ $(document).on('ready page:load', function (){
 
 function resizeContainer() {
   var value = $(window).height() ;
-  $('#welcome-cover,#welcome-cover-mask, #welcome-how-work, #welcome-made-for-who, #welcome-professionals, #welcome-sign-up, #welcome-contact-us').height(value);
+  $('#welcome-cover,#welcome-cover-mask, #welcome-how-work, #welcome-made-for-who, #welcome-professionals, #welcome-sign-up, #welcome-contact-us, #welcome-workshops').height(value);
   $('#welcome-how-work').css("margin-top",value);
   $('#welcome-content').css("margin-top",value - $('#welcome-content').height() + 50);
 }
