1349e109ca037cbdf5b9ad6f2416fa6355040c59
deivisson <deivisson.bruno@gmail.com>
2016-08-15 22:09:06 -0300

    Workshop Promotions

--- app/assets/javascripts/common/photos.js
+++ app/assets/javascripts/common/photos.js
@@ -30,7 +30,8 @@ function photoView(){
 		  		$(".ui-widget-overlay").css("opacity","1");
 		  		adjustLayout();
 		  		PhotoAvoidRightClick("#photo-picture");
-	  			$("#photo-view-modal-close-button").click(function() {
+	  			$("#photo-view-modal-close-button").click(function(e) {
+	  				e.preventDefault();
 		  			$("#photo-modal-dialog").dialog( "close" );
 					});
 		  });
